451f48a0b147ac0532fb97e1de1c0a8baecf1f8f
doyougnu <jeffrey.young@iohk.io>
2022-09-01 14:16:08 -0400

    ghc compilation strings

--- programs/emacs/doom/modules/+editor/multi-compile/config.el
+++ programs/emacs/doom/modules/+editor/multi-compile/config.el
@@ -5,6 +5,9 @@
   (map! :leader
         :map doom-leader-code-map
         "c c" #'multi-compile-run)
+  (setq multi-compile-template
+        (append multi-compile-template
+                '(("%test" . "Which test to test?: "))))
   (setq multi-compile-alist
         (append multi-compile-alist
                 '(
@@ -17,5 +20,13 @@
                        (projectile-root-bottom-up buffer-file-name))
                       ("Perf Build"     "nix-shell --pure --run 'hadrian/build -j12 --flavour=perf'"
                        (projectile-root-bottom-up buffer-file-name))
+                      ("JS Configure"     "nix-shell --pure --run 'emconfigure ./configure --target=js-unknown-ghcjs'"
+                       (projectile-root-bottom-up buffer-file-name))
+                      ("JS Build"     "nix-shell --run 'hadrian/build -j12 --flavour=quick-js+omit_pragmas --build-root=_js-backend --bignum=native --docs=none \"stage1.*.ghc.hs.opts += -ddump-stg-cg -ddump-js -ddump-to-file\"'"
+                       (projectile-root-bottom-up buffer-file-name))
+                      ("JS Test"     "nix-shell --run 'hadrian/build -j12 --flavour=quick-js+omit_pragmas --build-root=_js-backend --bignum=native --docs=none \"stage1.*.ghc.hs.opts += -ddump-stg-cg -ddump-js -ddump-to-file\" --only=%test'"
+                       (projectile-root-bottom-up buffer-file-name))
                       ))
                   ))))
+
+;;; config.el ends here
