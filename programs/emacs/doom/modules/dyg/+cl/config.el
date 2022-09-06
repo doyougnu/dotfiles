bc62a303bef51ed8032917f6df9f04b25bdf41ea
doyougnu <jeffrey.young@iohk.io>
2022-09-05 22:42:36 -0400

    autofill for sly

--- programs/emacs/doom/modules/dyg/+cl/config.el
+++ programs/emacs/doom/modules/dyg/+cl/config.el
@@ -7,10 +7,13 @@
   :init
   (add-to-list 'sly-contribs 'sly-asdf 'append)
 
+  :config
   (map! :map (sly-mode-map sly--completion-transient-mode-map sly--completion-display-map)
         :nvi "C-k" #'sly-prev-completion
         :nvi "C-j" #'sly-next-completion
         :nvi "C-l" #'sly-choose-completion)
+
+  (add-hook! 'sly-mode-hook #'auto-fill-mode)
 
   (setq doom-localleader-key ","
         doom-localleader-alt-key "C-."
