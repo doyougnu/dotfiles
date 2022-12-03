2219d0b9d46da4201c566461271db4581b524597
doyougnu <jeffrey.young@iohk.io>
2022-12-03 11:42:02 -0500

    better debug keys

--- programs/emacs/doom/modules/+lang/+elisp/config.el
+++ programs/emacs/doom/modules/+lang/+elisp/config.el
@@ -14,6 +14,15 @@
   "d" #'eval-defun
   "c" #'ielm/clear)
 
+ (:prefix ("d" . "debug")
+          "i" #'+emacs-lisp/edebug-instrument-defun-on
+          "I" #'+emacs-lisp/edebug-instrument-defun-off
+          "A" #'edebug-all-defuns
+          "a" #'edebug-all-forms
+          "m" #'edebug-set-initial-mode
+          "b" #'edebug-set-breakpoint
+          "B" #'edebug-set-conditional-breakpoint)
+
  (:prefix ("h" . "Help")
   "h" #'helpful-at-point)
 
