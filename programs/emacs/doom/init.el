6ef0654834841fbf83b5c3cc8fd399e609369ccb
doyougnu <jeffrey.young@iohk.io>
2023-04-20 10:26:18 -0400

    some cleanup

--- programs/emacs/doom/init.el
+++ programs/emacs/doom/init.el
@@ -36,7 +36,7 @@
        hl-todo                ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
-       ligatures         ; ligatures and symbols to make your code pretty again
+       ;; ligatures         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        ;; modeline    ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink cursor line after big motions
@@ -180,9 +180,9 @@
        :app
        ;;calendar
        ;;emms
-       everywhere                       ; *leave* Emacs!? You must be joking
+       ;; everywhere                       ; *leave* Emacs!? You must be joking
        irc                              ; how neckbeards socialize
-       (rss +org)                       ; emacs as an RSS reader
+       ;; (rss +org)                       ; emacs as an RSS reader
        ;;twitter           ; twitter client https://twitter.com/vnought
 
        :config
