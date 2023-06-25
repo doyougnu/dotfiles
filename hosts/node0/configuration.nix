e55835e9fc3f32791eefb5c965ea3b16d7096b08
doyougnu <jeffrey.young@iohk.io>
2023-06-25 10:00:20 -0400

    no user

--- hosts/node0/configuration.nix
+++ hosts/node0/configuration.nix
@@ -97,7 +97,6 @@
     useLegacyConfig    = false;
     openFirewall       = true;
     confOptions.useSSL = true;
-    user = "node0";
 
     config = {
       LoadModule = [ "adminlog" "certauth" ];
