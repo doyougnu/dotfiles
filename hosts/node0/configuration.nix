9f884bdc664f1b282d1fe78395a928fa0137bf33
doyougnu <jeffrey.young@iohk.io>
2023-06-25 15:00:05 -0400

    fix user perms

--- hosts/node0/configuration.nix
+++ hosts/node0/configuration.nix
@@ -64,7 +64,7 @@
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.node0 = {
-    isSystemUser = true;
+    isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" "audio" "pulse" "znc" ];
     group = "node0";
     home  = "/home/node0";
