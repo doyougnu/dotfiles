77bc2c78f9a7301c64f1863df5891ec0624fcda2
doyougnu <jeffrey.young@iohk.io>
2023-08-10 12:47:49 -0400

    add cdlatex, tweak latex settings

--- programs/emacs/doom/modules/+lang/+latex/config.el
+++ programs/emacs/doom/modules/+lang/+latex/config.el
@@ -2,9 +2,27 @@
 
 (after! latex
 
+  (setq TeX-auto-save t
+        TeX-parse-self t
+        TeX-debug-bad-boxes t)
+
+  (add-hook! LaTeX-mode-hook 'prettify-symbols-mode)
+  (add-hook! latex-mode-hook 'prettify-symbols-mode)
+  (add-hook! LaTeX-mode-hook #'turn-on-auto-fill)
+  (add-hook! latex-mode-hook #'turn-on-auto-fill)
+
   ;;;;;;;;;;;;;;;;;;;;;;;;; Custom key binds ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   (map!
    (:map (latex-mode-map LaTeX-mode-map)
     :localleader
+    "i" #'LaTeX-insert-item
     "e" #'LaTeX-environment
-    "E" #'LaTeX-close-environment)))
+    "E" #'LaTeX-close-environment
+    "o" #'TeX-fold-paragraph
+    "O" #'TeX-fold-clearout-paragraph
+
+    "=e" #'LaTeX-fill-environment
+    "=p" #'LaTeX-fill-paragraph
+    "=s" #'LaTeX-fill-section
+    "=n" #'LaTeX-fill-newline
+    )))
