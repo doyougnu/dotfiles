8bd4f442521cdde37e2d23727b49d9632cbdb248
doyougnu <jeffrey.young@iohk.io>
2023-08-09 20:11:36 -0400

    add some packages

--- programs/emacs/doom/init.el
+++ programs/emacs/doom/init.el
@@ -46,8 +46,8 @@
        ;;tabs              ; a tab bar for Emacs
        ;; treemacs         ; a project drawer, like neotree but cooler
        unicode          ; extended unicode support for various languages
-       ;; vc-gutter        ; vcs diff in the fringe
-       ;; vi-tilde-fringe  ; fringe tildes to mark beyond EOB
+       vc-gutter        ; vcs diff in the fringe
+       vi-tilde-fringe  ; fringe tildes to mark beyond EOB
        window-select     ; visually switch windows
        workspaces       ; tab emulation, persistence & separate workspaces
        zen               ; distraction-free coding or writing
@@ -55,7 +55,7 @@
        :editor
        (evil +everywhere)             ; come to the dark side, we have cookies
        file-templates                 ; auto-snippets for empty files
-       ;; fold                           ; (nigh) universal code folding
+       fold                           ; (nigh) universal code folding
        ;;(format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;; lispy            ; vim for lisp, for people who don't like vim
