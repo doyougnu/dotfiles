c9ab4325a339e3891627c82f0daa1883d068e634
doyougnu <jeffrey.young@iohk.io>
2023-11-28 18:15:37 -0500

    add ansi colors to babel

--- programs/emacs/doom/modules/dyg/+org/autoload.el
+++ programs/emacs/doom/modules/dyg/+org/autoload.el
@@ -378,3 +378,13 @@
   (interactive)
   (org-insert-comment)
   (evil-insert 1))
+
+;;;###autoload
+(defun dyg/babel-ansi ()
+  (when-let ((beg (org-babel-where-is-src-block-result nil nil)))
+    (save-excursion
+      (goto-char beg)
+      (when (looking-at org-babel-result-regexp)
+        (let ((end (org-babel-result-end))
+              (ansi-color-context-region nil))
+          (ansi-color-apply-on-region beg end))))))
--- programs/emacs/doom/modules/dyg/+org/config.el
+++ programs/emacs/doom/modules/dyg/+org/config.el
@@ -277,6 +277,7 @@
   (add-to-list 'org-structure-template-alist '("si" . "src scheme"))
 
   (add-hook! org-babel-after-execute-hook 'org-redisplay-inline-images)
+  (add-hook 'org-babel-after-execute-hook 'dyg/babel-ansi)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;; Custom key binds ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   (map!
