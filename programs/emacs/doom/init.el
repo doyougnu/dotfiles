adfb6d63b4211f62eedae0a7f7cbce029501096e
doyougnu <jeffrey.young@iohk.io>
2023-11-29 07:54:08 -0500

    add racket

--- programs/emacs/doom/init.el
+++ programs/emacs/doom/init.el
@@ -156,14 +156,14 @@
        ;;purescript        ; javascript, but functional
        (python +tree-sitter)              ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
-       ;;racket            ; a DSL for DSLs
+       racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
-       (scheme +tree-sitter +gauche +chez +racket)   ; a fully conniving family of lisps
+       (scheme +tree-sitter +gauche +chez)   ; a fully conniving family of lisps
        (sh +tree-sitter)     ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
        ;;solidity          ; do you need a blockchain? No.
