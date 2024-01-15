e6c7568960957fa0aa77e6ffa25b167ce87caff7
doyougnu <jeffrey.young@iohk.io>
2024-01-14 20:14:52 -0500

    doom: add pomodoro, modeline

--- programs/emacs/doom/init.el
+++ programs/emacs/doom/init.el
@@ -38,7 +38,7 @@
        ;;indent-guides     ; highlighted indent columns
        ;; ligatures         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
-       ;; modeline    ; snazzy, Atom-inspired modeline, plus API
+       modeline    ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints                ; highlight the region an operation acts on
@@ -150,7 +150,7 @@
        ;;nim               ; python + lisp at the speed of c
        (nix +tree-sitter +lsp)          ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +pretty +roam2 :tree-sitter)    ; organize your plain life in plain text
+       (org +pretty +roam2 +tree-sitter +pomodoro)    ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
