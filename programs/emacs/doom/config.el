80a3d5ba8adc7145039e1c530a41e4710ac42b06
doyougnu <jeffrey.young@iohk.io>
2024-01-15 09:06:12 -0500

    desktop: new theme

--- programs/emacs/doom/config.el
+++ programs/emacs/doom/config.el
@@ -47,7 +47,7 @@
 
 (if (equal (getenv "EMACS_HOST") "framework")
     (setq doom-theme 'doom-gruvbox-light)
-    (setq doom-theme 'wheatgrass))
+    (setq doom-theme 'doom-sourcerer))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
