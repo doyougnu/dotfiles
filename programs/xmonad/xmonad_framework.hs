e6b5406c1f6493e51937013e3d9e466a082ec5fb
doyougnu <jeffrey.young@iohk.io>
2024-04-24 21:43:49 -0400

    keyboard: change to dvorak programmer

--- programs/xmonad/xmonad_framework.hs
+++ programs/xmonad/xmonad_framework.hs
@@ -416,7 +416,8 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
   -- mod-[1..9], Switch to workspace N
   -- mod-shift-[1..9], Move client to workspace N
   [((m .|. modMask, k), windows $ f i)
-      | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+      | (i, k) <- zip (XMonad.workspaces conf)
+        [xK_ampersand, xK_bracketleft, xK_braceleft,xK_braceright,xK_parenleft, xK_equal]
       , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
   -- ++
 
